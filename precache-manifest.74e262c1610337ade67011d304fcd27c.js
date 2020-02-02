ee6e41f431c525cccff1f6036713e41a8d5bde32
Sam Collins <sam@scollins.co>
2020-02-02 12:34:17 +0000

    Updates

---
+++ precache-manifest.74e262c1610337ade67011d304fcd27c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b2b22793a6770a84e1bfda5fa4ccacd",
+    "url": "/wsart-links/index.html"
+  },
+  {
+    "revision": "1b17d809a4bed8555704",
+    "url": "/wsart-links/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "b7f64409e4b279f6feb0",
+    "url": "/wsart-links/static/js/2.eda41585.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/wsart-links/static/js/2.eda41585.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1b17d809a4bed8555704",
+    "url": "/wsart-links/static/js/main.a3f587c1.chunk.js"
+  },
+  {
+    "revision": "8b89afc4588f97ed8141",
+    "url": "/wsart-links/static/js/runtime-main.d98791fa.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/wsart-links/static/media/logo.5d5d9eef.svg"
+  }
+]);
